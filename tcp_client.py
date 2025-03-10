6510120a9dcec697316b4efb69642aabd166212a
benimbriaco1 <benimbriaco06@gmail.com>
2025-03-10 13:40:50 -0400

    TCP client

--- tcp_client.py
+++ tcp_client.py
@@ -5,4 +5,21 @@ target_host = "www.google.com"
 target_port = 80
 
 # creating a socket object
+# AF_INET refers to the ipv4 address family
+# SOCK_STREAM indicates TCP socket type
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# connecting our client
+client.connect((target_host, target_port))
+
+# send data
+client.send((b"GET / HTTP/1.1\r\nHost: www.google.com\r\n\r\n"))
+
+# receive the server's response (up to 4096 bytes)
+response = client.recv(4096)
+
+# decode raw bytes into readable format
+print(response.decode())
+
+# close our client
+client.close()
